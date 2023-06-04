7187b08eb0ab1ece82963c06c03bc9622424c107
Regina <rmyachina@mail.ru>
2023-06-04 18:44:26 +0300

    add task Webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+            //filename: 'main.html'
+        }),
+        new MiniCSSExtractPlugin(),
+    ]
+}
